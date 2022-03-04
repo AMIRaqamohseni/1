cf84ea4dac636f9b4b0c3b67657bb7009c3b21e8
Amir aqamohseni <100798123+AMIRaqamohseni@users.noreply.github.com>
2022-03-04 22:06:42 +0000

    Add files via upload

---
+++ untitled/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int one,two;
+    float three,four = 1.5;
+    printf("add avalie ra vared konid  ");
+    scanf("%i",&one);
+    printf("add Akhar ra vared konid ");
+    scanf("%i",&two);
+    three = (two - one)*four;
+    printf(" harkat Shoma %i ast keraye shoma %f  mibshad kraye kol %f ast",two-one,four,three);
+    return 0;
+}
